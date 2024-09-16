78ec08cecff1ff3ec0e3939ca40407ac9b7f48a2
Muhammad Wildan Dhiya Ulhaq <mwildab15@gmail.com>
2024-09-17 00:29:28 +0700

    update dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,8 @@ function App() {
       <Sidebar />
 
       <div className="p-4 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg h-fit dark:border-gray-700">
-          <div className='bg-red-400 max-h-full rounded-md p-4 h-[600px] overflow-auto mb-6'>
+        <div className="p-4 border-2 border-gray-200 rounded-lg h-fit dark:border-gray-700">
+          <div className='bg-slate-800 max-h-full rounded-md p-4 h-[600px] overflow-auto mb-6'>
             {loading ? (
               <div className='loader bg-neutral-100 h-fit rounded-md p-4'>
                 <hr />

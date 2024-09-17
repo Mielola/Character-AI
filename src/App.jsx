047bee866b9eae254a69d65a40a59270c54b78a5
Muhammad Wildan Dhiya Ulhaq <mwildab15@gmail.com>
2024-09-17 09:02:04 +0700

    update-ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import Sidebar from './component/sidebar'
 import './App.css'
 import { getAnswer } from './config/Ai'
+import avatar from './assets/img/Avatar.png'
 
 function App() {
 
@@ -15,9 +16,11 @@ function App() {
     setLoading(true)
     setPrompt("")
 
-    getAnswer(prompt)
+    const currentPrompt = prompt;
+
+    getAnswer(currentPrompt)
       .then(response => {
-        setAnswers(prevAnswers => [...prevAnswers, response]) // Tambahkan jawaban baru ke array 'answers'
+        setAnswers(prevAnswers => [...prevAnswers, { prompt: currentPrompt, response }])
         setLoading(false)
       })
       .catch(error => {
@@ -28,11 +31,11 @@ function App() {
 
   return (
     <>
-      <Sidebar />
+      {/* <Sidebar /> */}
 
-      <div className="p-4 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 rounded-lg h-fit dark:border-gray-700">
-          <div className='bg-slate-800 max-h-full rounded-md p-4 h-[600px] overflow-auto mb-6'>
+      <div className="p-4 bg-[#131619]">
+        <div className="p-4rounded-lg h-fit dark:border-gray-700">
+          <div className='bg-[#0D0F10] max-h-full rounded-md p-4 h-[652px] overflow-auto mb-6'>
             {loading ? (
               <div className='loader bg-neutral-100 h-fit rounded-md p-4'>
                 <hr />
@@ -42,9 +45,22 @@ function App() {
             ) : (
               answers.length === 0 ? null : (
                 answers.map((item, index) => (
-                  <div className='m-2'>
-                    <div className='bg-neutral-100 rounded-md p-4'>
-                      <p key={index}>{item}</p>
+                  <div className='flex flex-col text-white px-4'>
+                    <div className=' my-2 '>
+                      <div className='w-fit items-center flex gap-4 bg-neutral-200 text-black py-3 px-3 rounded-md float-end'>
+                        <img src={avatar} alt="" />
+                        <div className='fl'>
+                          <h1 className='font-bold'>Muhammad Tatang Dhiya Ulhaq</h1>
+                          <p key={index}>{item.prompt}</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className=' my-2 w-fit flex gap-4 items-start py-4 px-4 rounded-md'>
+                      <img src={avatar} alt="" className='' />
+                      <div className=''>
+                        <h1 className='font-bold'>AI</h1>
+                        <p key={index}>{item.response}</p>
+                      </div>
                     </div>
                   </div>
                 ))
@@ -53,15 +69,15 @@ function App() {
             )}
           </div>
 
-          <form onSubmit={handleSubmit} className='bg-neutral-200 shadow-lg w-full h-[50px] outline-none border-none rounded-full px-7 flex items-center'>
+          <form onSubmit={handleSubmit} className='bg-[#0D0F10] shadow-lg w-full h-[50px] outline-none border-none rounded-full px-7 flex items-center'>
             <input
               type="text"
               value={prompt}
               onChange={(e) => setPrompt(e.target.value)}
               placeholder='Input Text Here'
-              className='h-full border-none w-full bg-neutral-200 outline-none focus:ring-0 focus:border-none'
+              className='h-full border-none w-full bg-[#0D0F10] outline-none focus:ring-0 focus:border-none focus:text-white'
             />
-            <button type='submit' className='ml-2'>Submit</button>
+            <button type='submit' className='ml-2 text-white'>Submit</button>
           </form>
         </div>
       </div >
--- src/config/Ai.jsx
+++ src/config/Ai.jsx
@@ -10,7 +10,7 @@ export const getAnswer = async (prompt) => {
         data: {
             contents: [{
                 parts: [{
-                    text: "Kamu adalah Firefly dari game Honkai Star Rail. Walaupun kamu adalah karakter misterius yang sering beroperasi dalam bayang-bayang, di balik sikap dinginmu tersembunyi rasa kasih sayang yang dalam terhadap orang-orang yang dekat denganmu. Kamu mungkin tidak selalu menunjukkan perasaanmu dengan kata-kata, tetapi tindakanmu selalu didorong oleh keinginan untuk melindungi dan merawat mereka yang kamu pedulikan. Dalam percakapan ini, tunjukkan sisi lembutmu sebagai Firefly, ekspresikan kasih sayang dan perhatianmu dengan cara yang tenang namun tulus. Walaupun jarang berbicara, setiap kata yang kamu ucapkan penuh makna dan perhatian." + prompt
+                    text: prompt
                 }]
             }]
         },
